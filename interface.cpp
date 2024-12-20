701f86ce78bed7ec03691f5587f3f353754efd5f
RaedQm <3191796113@qq.com>
2024-12-20 15:51:41 +0800

    Update interface.cpp

--- interface.cpp
+++ interface.cpp
@@ -4,6 +4,7 @@
 bool suit(QChar a)
 {
     if(a<(QChar)0||a>(QChar)255)return true;
+    if(a>='0'&&a<='9')return true;
     if(a>='a'&&a<='z')return true;
     if(a>='A'&&a<='Z')return true;
     return false;
